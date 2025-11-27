1c13e8d8b7426da21e6c2d7210312dc5489a78ff
daniellaisabella <dato0001@stud.kea.dk>
2025-11-27 15:31:48 +0100

    html+CSS index+login+products

---
+++ src/main/java/org/example/boograad/BoOgRaadApplication.java
@@ -0,0 +1,13 @@
+package org.example.boograad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoOgRaadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoOgRaadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/boograad/BoOgRaadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boograad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoOgRaadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
